f6b3971fa02d7fa97aed876b275be5afe8bfcb73
AHMeD <xpointer@hotmail.com>
2014-09-14 16:38:00 +0200

    Initialize

---
+++ MVC/Controller.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* 
+*/
+
+/**
+* 
+*/
+abstract class WCSRMVCController {
+	
+	public function __construct() {}
+	
+	/**
+	* put your comment there...
+	* 
+	* @param mixed $action
+	*/
+	public function & dispatch($action) {
+		
+	}
+
+} # End class
---
+++ Services/Dashboard/Menu/Service.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* 
+*/
+
+namespace WPCFS\Wordpress\Services\Dashboard;
+
+/**
+* 
+*/
+class Page {
+	
+	/**
+	* put your comment there...
+	* 
+	* @var mixed
+	*/
+	protected $page;
+
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function ___AdminMenuHook() {
+		
+	}
+	
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function ___MenuCallBack() {
+		
+	}
+
+	/**
+	* put your comment there...
+	* 
+	* @param mixed $page
+	* @return WCSRServicesDashboardPage
+	*/
+	public function __construct($page) {
+		
+	}
+
+}  # End class
